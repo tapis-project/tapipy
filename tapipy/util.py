d076c41e4d020ec85c1d94fd3453c22e4d7c316f
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-05-16 16:23:35 -0500

    add recursive TapisResult serializer

--- tapipy/util.py
+++ tapipy/util.py
@@ -4,3 +4,19 @@ class AttrDict(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
+
+def serialize_result(result):
+        if type(result) in [str, int, float, type(None), bool]: return result
+        
+        if type(result) == list:
+            modified_result = []
+            for res in result:
+                modified_result.append(serialize_result(res))
+
+            return modified_result
+        
+        modified_result = result.__dict__
+        for prop in modified_result:
+            modified_result[prop] = serialize_result(modified_result[prop])
+
+        return modified_result
